26a0fda544e66b49eab0f17ed78a8417a3de2c88
dieterch <dieter.chvatal@gmail.com>
2024-03-01 21:05:56 +0100

    password field style

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -8,6 +8,8 @@ const password = ref('');
 
 const props = defineProps({ authenticated: Boolean, hash: String })
 const emit = defineEmits(['authenticated'])
+const show1 = ref(false)
+const passord = ref('Password')
 
 const login = async() => {
     //console.log('login', password.value)
@@ -39,7 +41,15 @@ onMounted(() => {
             <v-form fast-fail @submit.prevent="login">
                 <!--v-text-field  variant="underlined" v-model="username" label="User Name"></v-text-field-->
 
-                <v-text-field variant="underlined" v-model="password" label="password"></v-text-field>
+                <v-text-field 
+                    variant="underlined" 
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :type="show1 ? 'text' : 'password'"
+                    v-model="password" 
+                    label="Password" 
+                    clearable
+                    @click:append="show1 = !show1"
+                ></v-text-field>
                 <!--a href="#" class="text-body-2 font-weight-regular">Forgot Password?</a-->
 
                 <v-btn type="submit" variant="outlined" color="primary" block class="mt-2">Submit</v-btn>
